86e7e14926ef634c7653acf4c94d17f266d02544
LautyNC <lauticolque@gmail.com>
2024-10-22 14:19:25 -0300

    conexion a sqlite

--- src/main/java/com/mycompany/proyectoprogramacion/ProyectoProgramacion.java
+++ src/main/java/com/mycompany/proyectoprogramacion/ProyectoProgramacion.java
@@ -1,8 +1,30 @@
 package com.mycompany.proyectoprogramacion;
-
+import java.sql.*;
 public class ProyectoProgramacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Connection conn = null;
+        try {
+            //cargar el driver
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:test01.sqlite");
+
+            if (conn != null)
+		System.out.println("Conexion a base de datos ... Ok");
+            else
+                System.out.println("Conexion a base de datos: problemas!");
+
+        conn.close();        //cerrar la BD
+
     }
+        catch (SQLException a) {
+            System.out.println(a);
+        }
+        catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+    }
+  }
 }
