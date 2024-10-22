e101d0d5bf4817ce959653e3001172b10a662669
LautyNC <lauticolque@gmail.com>
2024-10-22 14:13:34 -0300

    primer commit

---
+++ src/main/java/com/mycompany/proyectoprogramacion/ProyectoProgramacion.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyectoprogramacion;
+
+public class ProyectoProgramacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
